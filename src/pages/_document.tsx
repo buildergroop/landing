9f727250385ffaa0ec242d93b52b06850c86efc2
Aaryaman Maheshwari <hey@aaryaman.io>
2022-01-29 01:27:26 +0530

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,24 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favicon/favicon-16x16.png"
+          />
         </Head>
         <body className="bg-dark text-white overflow-x-hidden">
           <Main />
