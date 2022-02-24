8b3034338084ddd021448327060bbabbf0f1d86b
Aaryaman Maheshwari <hey@aaryaman.io>
2022-02-25 03:06:00 +0530

    Update hacks.tsx

--- src/pages/forms/hacks.tsx
+++ src/pages/forms/hacks.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 export default function BuilderHacks() {
   useEffect(() => {
-    window.location.href = "https://nssdo58wcfn.typeform.com/to/HJqEqI1k";
+    window.location.href = "https://forms.gle/Lcn2MYdFEDFtiEpC6";
   });
 
   return <>Taking you to the form</>;
