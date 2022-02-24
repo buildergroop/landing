720cf5d9ac337c753a99669548ffb5b67cd42574
Aaryaman Maheshwari <hey@aaryaman.io>
2022-02-25 01:50:06 +0530

    add forms/hacks

---
+++ src/pages/forms/hacks.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+
+export default function BuilderHacks() {
+  useEffect(() => {
+    window.location.href = "https://nssdo58wcfn.typeform.com/to/HJqEqI1k";
+  });
+
+  return <>Taking you to the form</>;
+}
